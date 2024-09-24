0cede6c825ac9c00f0d56619020154b4c400965a
Yuvaraja T <54367219+yuvarajat@users.noreply.github.com>
2024-09-24 19:30:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 })(jQuery);
 
 
-$(document).on('click', function(){
+$(document).on('load', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
