c2a4ba1cf8974d3fabd73ddb69c1b9d5b328fbfb
Yuvaraja T <beemyuva@gmail.com>
2024-10-12 01:39:58 +0530

    Updated heart and click here to open text

--- js/scriptEnvelope.js
+++ js/scriptEnvelope.js
@@ -1,36 +1,67 @@
-window.onload = function() {
-    $(document).ready(function() {
-        var envelope = $('#envelope');
-        
-        // Automatically open the envelope after 2 seconds (2000 ms)
-        setTimeout(function() {
-            openEnvelope();
-        }, 1000);
-        
-        function openEnvelope() {
-            envelope.addClass('open')
-                .removeClass('close');
-            // Smooth transition and redirect
-            setTimeout(function() {
-                $('body').addClass('fade-out'); // Add fade-out class
-                setTimeout(function() {
-                    window.location.href = 'invitation.html'; // Redirect after fade-out
-                }, 200); // Delay to match the fade-out duration
-            }, 3000); // Initial delay before fade-out
-        }
+window.onload = function () {
+  $(document).ready(function () {
+    var envelope = $("#envelope");
+    var sealHeart = $("#sealHeart");
+    var clickHereText = $("#clickHereText");
+    var toOpen = $("#toOpen");
+    var clickTimeout;
+
+     // Load the content of invitation.html
+     fetch("invitation.html")
+     .then(response => {
+         if (!response.ok) {
+             throw new Error("Network response was not ok");
+         }
+         return response.text();
+     })
+     .then(html => {
+         document.getElementById("invitationContent").innerHTML = html; // Load HTML into a specific element
+     })
+     .catch(error => {
+         console.error("Error loading the invitation:", error);
+     });
+
+    //Preload decorations images
+    var decorationImgLeft = new Image();
+    decorationImgLeft.src = "../assets/img/left.png";
+
+    var decorationImgRight = new Image();
+    decorationImgRight.src = "../assets/img/right.png";
+
+    // Preload background image for invitation.html
+    var bgImage = new Image();
+    bgImage.src = "../assets/img/DSC_7560.webp"; // Set the correct image path here
+
+    // $(document).on('click', function(){
+    //     document.getElementById("my_audio").play();
+    // });
+
+    // Set a timeout to show the "Click Here" text after 3 seconds
+    clickTimeout = setTimeout(function () {
+      clickHereText.show();
+      toOpen.show();
+    }, 3000); // 3 seconds
+
+    // Open the envelope when it is clicked
+    envelope.on("click", function () {
+      clearTimeout(clickTimeout); // Clear the timeout if the user clicks
+      clickHereText.hide(); // Hide the "Click Here" text
+      toOpen.hide();
+      sealHeart.hide();
+      openEnvelope();
     });
 
-    console.log("Entire page is fully loaded");
+    function openEnvelope() {
+      envelope.addClass("open").removeClass("close");
+      // Smooth transition and redirect
+      setTimeout(function () {
+        $("body").addClass("fade-out"); // Add fade-out class
+        setTimeout(function () {
+          window.location.href = "invitation.html"; // Redirect after fade-out
+        }, 200); // Delay to match the fade-out duration
+      }, 3000); // Initial delay before fade-out
+    }
+  });
+
+  console.log("Entire page is fully loaded");
 };
-
-const guestName = document.getElementById("guestName");
-        
-const queryString = window.location.search;
-
-// Create a new URLSearchParams object
-const params = new URLSearchParams(queryString);
-
-// Retrieve specific parameters using .get()
-let finalname=atob(params.get('invite')||"RGVhciBHdWVzdA==");
-        
-guestName.innerText = finalname;
